dd6bee1c76a208f69c3baf59b12a9bedc98bd5e7
Kevin Chau <kevin.chau57@gmail.com>
2022-07-01 16:34:34 -0700

    Added parsing functions for uniqlo products

--- model/uniqloprod.js
+++ model/uniqloprod.js
@@ -1,15 +1,42 @@
 module.exports = class UniqloProduct {
     constructor(PID, prodSRC, stockSRC) {
+        this.pid = PID;
         this.url = "https://www.uniqlo.com/us/en/products/" + PID;
-        this.prodData = prodSRC;
-        this.stockData = stockSRC;
-        this.pic = this.parseImgs(this.prodData);
+        this.salestatus = this.parseSaleBool(prodSRC);
+        this.sizes = this.parseSizes(prodSRC)
+        this.stockLevel = this.parseStockLevel(stockSRC)
+        this.pic = this.parseImgs(prodSRC);
     }
-    parseImgs(prodSRC) {
+    parseImgs(jsondata) {
         const photos = [];
-        for (const key in prodSRC.result.images.main) {
-            photos.push(prodSRC.result.images.main[key].image);
+        for (const key in jsondata.result.images.main) {
+            photos.push(jsondata.result.images.main[key].image);
         }
-        return photos
+        return photos;
+    }
+    parseSaleBool(jsondata) {
+        for (const key in jsondata.result.l2s) {
+            if (jsondata.result.l2s[key].sales == true) {
+                return true
+            }
+        }
+        return false;
+    }
+    parseSizes(jsondata) {
+        const sizes = [];
+        for (const size in jsondata.result.sizes) {
+            sizes.push(jsondata.result.sizes[size].name);
+        }
+        return sizes;
+    }
+    parseStockLevel(jsondata) {
+        const stockLevels = [];
+        let totalStock = 0;
+        for (const key in jsondata.result) {
+            totalStock += jsondata.result[key].quantity;
+            stockLevels.push(jsondata.result[key].quantity);
+        }
+        stockLevels.unshift(totalStock);
+        return stockLevels;
     }
 }

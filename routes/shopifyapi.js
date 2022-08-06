496018d0dc08364b50aded273b02a52d1107d639
kevinxd-xd <57789343+kevinxd-xd@users.noreply.github.com>
2022-08-05 19:24:22 -0700

    Fixed routes

--- routes/shopifyapi.js
+++ routes/shopifyapi.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 // Necessary Controllers
-const request = require('../controllers/shopifyrequest.js')
+const request = require('../controllers/requests/shopifyrequest.js')
 
 
 // Routes
--- routes/uniqloapi.js
+++ routes/uniqloapi.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 // Necessary Controllers
-const request = require('../controllers/uniqlorequest.js')
+const request = require('../controllers/requests/uniqlorequest.js')
 
 // Routes
 router.use('', async (req, res) => {

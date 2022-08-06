ebf45f0b898493cc37f11b3eb53de142df2a1e76
kevinxd-xd <57789343+kevinxd-xd@users.noreply.github.com>
2022-08-06 00:55:10 -0700

    Constructor and field changes

--- model/shopifyprod.js
+++ model/shopifyprod.js
@@ -1,10 +1,10 @@
 module.exports = class ShopifyProduct {
-    constructor(url, src) {
+    constructor(url, variants, subnames, pics, productName) {
         this.URL = url;
-        this.vars = this.parseVariants(src);
-        this.subnames = this.parseSubNames(src);
-        this.pic = src.media[0].src;
-        this.prodName = src.title;
+        this.vars = variants;
+        this.subnames = subnames;
+        this.pic = pics;
+        this.prodName = productName;
     }
     parseVariants(source) {
         const variants = []
@@ -19,5 +19,12 @@ module.exports = class ShopifyProduct {
             subnames.push(source.variants[key].title);
         }
         return subnames;
+    }
+    setFields(url, src) {
+        this.URL = url;
+        this.vars = this.parseVariants(src);
+        this.subnames = this.parseSubNames(src);
+        this.pic = src.media[0].src;
+        this.prodName = src.title;
     }
 }

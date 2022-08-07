c771429a7be91374a8833e1fc95010adc7c09d2e
kevinxd-xd <57789343+kevinxd-xd@users.noreply.github.com>
2022-08-07 16:50:52 -0700

    Added UPDATE functionality and reduced dependencies

--- controllers/queries/shopifyquery.js
+++ controllers/queries/shopifyquery.js
@@ -1,9 +1,8 @@
 const db = require('../../db/index.js')
-const ShopifyProduct = require('../../model/shopifyprod')
-const ShopifyReq = require('../requests/shopifyrequest')
 
 module.exports = {
     /**
+     * READ
      * Gets the entry matching the link provided
      * @param {string} link - pass in a link and will return the shopifyprod that matches that link
      */
@@ -11,18 +10,8 @@ module.exports = {
         try {
             const res = await db.connect.query(`SELECT * FROM product_shopify WHERE url='${link}';`);
             if (res.rowCount < 1) {
-                console.log("No entry was found, making a request to corresponding server...");
-                const req = await ShopifyReq.getSource(link);
-                if (req) {
-                    console.log("Data received! Inserting into database...");
-                    await this.insertShopEntry(req);
-                    console.log("Relaying data to requester!");
-                    return req;
-                }
-                else {
-                    console.log("Bad link was provided! No results will be returned");
-                    return null;
-                }
+                console.log("No entry was found...");
+                return null;
             }
             else {
                 console.log("Successfully found at least 1 entry!");
@@ -30,23 +19,63 @@ module.exports = {
             }
         }
         catch (err) {
-            console.log(err.stack);
+            console.log("An error occured! No entry was found...");
         }
     },
 
     /**
+     * CREATE
      * This method inserts a shopifyprod object into the database
-     * @param {shopifyprod} prod - shopifyprod object that contains all of the info needed to insert into the database
+     * @param {shopifyprod} prod - a shopifyprod object with all the necessary fields
      */
     async insertShopEntry(prod) {
         try {
             const command = `INSERT into product_shopify (url, vars, subnames, picurl, prodname) VALUES ($1, $2, $3, $4, $5);`
-            const values = [prod.URL, prod.vars, prod.subnames, prod.pic, prod.prodName];
-            const res = db.connect.query(command, values);
+            const values = [prod.URL, prod.vars, prod.subnames, prod.picurl, prod.prodname];
+            const res = await db.connect.query(command, values);
             console.log("Successfully inserted entry!");
+            return 200;
         }
         catch (err) {
-            console.log(err.stack);
+            if (err.code == "23505") {
+                console.log("Duplicate entry was found! Insert failed...");
+            }
+            else {
+                console.log(err.stack);
+            }
         }
     },
+
+    /**
+     * UPDATE
+     * This method updates a shopifyprod object that already exists in the database
+     * @param {shopifyprod} prod - a shopifyprod object with all the necessary fields
+     */
+    async updateShopEntry(prod) {
+        try {
+            const command = `UPDATE product_shopify SET url = $1, vars = $2, subnames = $3, picurl = $4, prodname = $5 WHERE url = $1 RETURNING prodname;`
+            const values = [prod.URL, prod.vars, prod.subnames, prod.picurl, prod.prodname];
+            console.log("Attempting to update entry");
+            const resp = await db.connect.query(command, values);
+            
+            if (resp.rowCount < 1) {
+                console.log("Update failed...");
+                return null;
+            }
+            else { 
+                console.log("Succesfully updated entry!");
+                return 200;
+            }
+        }
+        catch (err) {
+            console.log("An error occured, update failed...");
+            console.log(err.stack);
+        }
+    }
+
+    /**
+     * DELETE
+     */
+
+
 }
--- controllers/queries/uniqloquery.js
+++ controllers/queries/uniqloquery.js
@@ -1,28 +1,17 @@
 const db = require('../../db/index.js')
-const UniqloProduct = require('../../model/uniqloprod')
-const UniqloReq = require('../requests/uniqlorequest')
 
 module.exports = {
     /**
+     * READ
      * Gets the entry matching the PID provided from Uniqlo
-     * @param {string} link - pass in a pid and will return the uniqloprod that matches that PID
+     * @param {string} PID - pass in a pid and will return the uniqloprod that matches that PID
      */
     async getUniqloEntry(PID) {
         try {
             const res = await db.connect.query(`SELECT * FROM product_uniqlo WHERE pid='${PID}';`);
             if (res.rowCount < 1) {
-                console.log("No entry was found, making a request to corresponding server...");
-                const req = await UniqloReq.getUniqlo(PID);
-                if (req) {
-                    console.log("Data received! Inserting into database...");
-                    await this.insertUniqloEntry(req);
-                    console.log("Relaying data to requester!");
-                    return req;
-                }
-                else {
-                    console.log("Bad PID was provided! No results will be returned");
-                    return null;
-                }
+                console.log("No entry was found...");
+                return null;
             }
             else {
                 console.log("Successfully found at least 1 entry!");
@@ -30,23 +19,60 @@ module.exports = {
             }
         }
         catch (err) {
-            console.log(err.stack);
+            console.log("An error occured! No entry was found...");
+            return null;
         }
     },
 
     /**
+     * CREATE
      * This method inserts a uniqloprod object into the database
      * @param {uniqloprod} prod - uniqloprod object that contains all of the info needed to insert into the database
      */
     async insertUniqloEntry(prod) {
         try {
             const command = `INSERT into product_uniqlo (pid, url, salestatus, sizes, stocklevel, pic) VALUES ($1, $2, $3, $4, $5, $6);`
-            const values = [prod.pid, prod.url, prod.salestatus, prod.sizes, prod.stockLevel, prod.pic];
-            const res = db.connect.query(command, values);
+            const values = [prod.pid, prod.url, prod.salestatus, prod.sizes, prod.stocklevel, prod.pic];
+            const res = await db.connect.query(command, values);
             console.log("Successfully inserted entry!");
         }
         catch (err) {
-            console.log(err.stack)
+            if (err.code == "23505") {
+                console.log("Duplicate entry was found! Insert failed...");
+                return null;
+            }
+            else {
+                console.log(err.stack);
+                return null;
+            }
+        }
+    },
+
+    /**
+     * UPDATE
+     * This method updates a matching uniqlo pid in the database
+     * @param {uniqloprod} prod 
+     * @returns status of the request - uniqloprod object that contains all of the info needed to insert into the database
+     */
+    async updateUniqloEntry(prod) {
+        try {
+            const command = `UPDATE product_uniqlo SET pid = $1, url = $2, salestatus = $3, sizes = $4, stocklevel = $5, pic = $6 WHERE pid = $1 RETURNING pid;`
+            const values = [prod.pid, prod.url, prod.salestatus, prod.sizes, prod.stocklevel, prod.pic];
+            const resp = await db.connect.query(command, values);
+
+            if (resp.rowCount < 1) {
+                console.log("Update failed...");
+                return null;
+            }
+            else { 
+                console.log("Succesfully updated entry!");
+                return 200;
+            }
+        }
+        catch (err) {
+            console.log("An error occured, update failed...");
+            console.log(err.stack);
+            return null;
         }
     },
 }
